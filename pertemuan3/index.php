9baf6d86f91b7795b1f56954ce55dbcd5788d222
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2023-10-13 10:11:40 +0700

    first commit

---
+++ pertemuan3/index.php
@@ -0,0 +1,13 @@
+<?php
+// for ($i = 0; $i <= 10; $i++) {
+//     echo "Pengulangan Ke $i \n";
+// }
+
+// membuat class Person 
+class Person
+{
+    // membuat property (variable)
+    public $nama;
+    public $alamat;
+    public $jurusan;
+}

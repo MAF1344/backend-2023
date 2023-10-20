388247cd301fe8829ad91406e69d1923fd246657
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2023-10-20 10:59:34 +0700

    Task - Animal(GET,POST,PUT,DELETE)

--- rest-api/app/Http/Controllers/AnimalController.php
+++ rest-api/app/Http/Controllers/AnimalController.php
@@ -6,28 +6,52 @@ use Illuminate\Http\Request;
 
 class AnimalController extends Controller
 {
+    protected $animal = ["Kucing", "Ayam", "Ikan"];
+
     public function index()
     {
-        $animal = ["Kucing","Ayam","Ikan"];
-        echo "Menampilkan Data Animal : <br> $animal";
+        echo "Menampilkan Data Animal : <br>";
+        foreach ($this->animal as $item) {
+            echo "~ $item <br>";
+        };
     }
 
     public function store(Request $request)
     {
-        echo "Nama Hewan : $request->nama";
-        echo "<br>";
-        echo "Menamahkan Data Animal";
+        echo "Data Animal : <br>";
+        foreach ($this->animal as $item) {
+            echo "~ $item <br>";
+        };
+        echo "Menambahkan Data Animal <br>";
+        echo "Nama Hewan Baru : $request->nama";
     }
 
     public function update(Request $request, $id)
     {
-        echo "Nama Hewan : $request->nama";
-        echo "<br>";  
-        echo "Merubah Data Animal id $id";
+        echo "Mengubah Data Animal : <br>";
+        if (isset($this->animal[$id])) {
+            $this->animal[$id] = $request->nama;
+
+            foreach ($this->animal as $item) {
+                echo "~ $item <br>";
+            };
+        } else {
+            echo "Data Animal id $id tidak ditemukan";
+        }
     }
 
     public function destroy($id)
     {
-        echo "Menghapus Data Animal $id";
+        if (isset($this->animal[$id])) {
+            unset($this->animal[$id]);
+        } else {
+            echo "Data Animal id $id tidak ditemukan";
+        }
+
+        echo "Menghapus Data Animal id $id <br>";
+        echo "Data Animal Yang Tersisa : <br>";
+        foreach ($this->animal as $item) {
+            echo "~ $item <br>";
+        };
     }
 }

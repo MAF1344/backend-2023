404e1cae06db8fb092b130572807d179a9e9228d
MAF1344 <muha22126ti@student.nurulfikri.ac.id>
2023-10-27 20:06:48 +0700

    Task-5 : Melanjutkan Membuat Method PUT dan DELETE

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -36,20 +36,64 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
-        $student = Student::create($request->all());
-        return response()->json($student, 201);
+        // Menangkap data request
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan,
+        ];
+
+
+        // Menggunakan model Student
+        $student = Student::create($input);
+
+        $data = [
+            'message' => 'Student is created succesfully',
+            'data' => $student,
+        ];
+
+        // Mengembalikan data (JSON) dan kode 201
+        return response()->json($data, 201);
     }
 
     public function update(Request $request, $id)
     {
-        $student = Student::findOrFail($id);
-        $student->update($request->all());
-        return response()->json($student, 201);
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
+
+        $student->nama = $request->input('nama', $student->nama);
+        $student->nim = $request->input('nim', $student->nim);
+        $student->email = $request->input('email', $student->email);
+        $student->jurusan = $request->input('jurusan', $student->jurusan);
+
+        $student->save();
+
+        $data = [
+            'message' => 'Student is updated successfully',
+            'data' => $student,
+        ];
+
+        return response()->json($data, 200);
     }
 
     public function destroy($id)
     {
-        Student::destroy($id);
-        return response()->json(null, 0);
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
+
+        $student->delete();
+
+        $data = [
+            'message' => 'Student has been deleted successfully',
+        ];
+
+        return response()->json($data, 200);
     }
 }
